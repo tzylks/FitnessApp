ccd31b7540b9060d7ab8d4925022bbfa8b721630
TrevorZylks <trevorzylks@gmail.com>
2021-09-12 00:54:18 -0700

    box shadow

--- client/src/components/Pricing.js
+++ client/src/components/Pricing.js
@@ -131,7 +131,7 @@ export default function Pricing() {
           {tiers.map((tier) => (
             // Enterprise card is full width at sm breakpoint
             <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
-              <Card style={{marginBottom: '100px'}}>
+              <Card style={{marginBottom: '100px', boxShadow: "0 19px 50px  rgba(120, 222, 199, .5)"}}>
                 <CardHeader
                   title={tier.title}
                   subheader={tier.subheader}
